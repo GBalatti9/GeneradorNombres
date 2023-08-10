88e639260ed22e41382f4bdb6c71abe3804e80ca
Balatti Gaston (Redaccion) <gbalatti@lanacion.com.ar>
2023-08-10 19:53:53 -0300

    Mas nombres

--- public/js/index.js
+++ public/js/index.js
@@ -1,12 +1,14 @@
+import { names } from "./data.js";
+
 window.onload = () => {
 
+    console.log("prueba");
+    let firstNames = names.firstNames;
+    let lastNames = names.lastNames;
     const container = document.getElementById("container");
     const inputBtn = document.getElementById("button");
     console.log(container);
     console.log(inputBtn);
-
-    const firstNames = ["Tornillo", "Benji", "Toshiba", "Lisandro", "Ricardo", "Betty", "Alicio", "Lenny", "Mabel", "Pablo", "El Doctor", "Chicho", "Rudo", "Sir Anthony", "Jenny", "Cannoli", "El Payaso", "Kevin", "Lázaro", "Creek Creek", "Darío", "Pipo", "Colo", "Betty", "Costillita", "Estigma", "Chico", "Juanpi", "Cramalót", "Boris", "René", "Claudia", "Marisa", "Abraham Bolt Bolt", "Mili", "El Gordo", "Jason", "Kiko", "Abel", "La Roca", "Aníbal Gilera", "Carminatti", "Tiki Tiki", "Kiko", "Amílcar", "Silvia", "Claudio", "Gabo","Tico", "Techno", "El Niño", "Gómez", "Renzo", "Coki", "Carlos", "Cristal", "Gustavo", "Culo"]
-    const lastNames = ["Goldstein", "Tratativa", "Bronderbol", "Aristimuñón", "Kakalardi", "Laválvula", "Renevoldi", "Culo", "Maluca Gómez", "Asecas", "Vulvini", "Verga", "Roncaglia", "Miller", "La Panza", "Tinkerbell", "Caquita", "Dolver", "Bimolti", "Cañanamo de Leche", "Barros Schebarros", "Túnel", "Vagino", "Dos Dientes", "D'Angelo", "Y Torbellino", "Trueno", "Binomio", "Vivot", "Timber", "Laborda", "Buscapleito", "Bombastic", "Sit and Down", "Trailer", "Prompter", "Dime Que Sí", "Del Bueno", "Van Der", "Meijide", "System", "Tres Cuerpos", "De Frensí", "Bensimon", "Cumshot", "Guglielmiclaudio", "Usandivago", "Astrazeneca", "Fusión 3000", "Croma", "Gómez", "Google", "Soundblaster", "Trompas", "Lancelot", "Colliver", "Gutierrez"]
 
 
     let indexFN = 0;
